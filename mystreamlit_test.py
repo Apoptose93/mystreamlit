e354b6d2cac399e618f3bb052309b6b84e91121b
apoptose <nicbra93@gmail.com>
2021-11-14 15:55:48 +0100

    multi-mode

--- mystreamlit_test.py
+++ mystreamlit_test.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 if __name__ == '__main__':
     x = st.slider("Select a value")
-    st.write(x, "squared is", x*x)
+    #st.write(x, "squared is", x*x)
+
+   # text = st.text_input("json")
+   # st.json(text)
